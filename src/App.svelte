afdf4bb38fd0c47cd0a115f4efc3b3a8ee94728e
Florian Trayon <git@florian-trayon.fr>
2025-05-03 20:53:43 +0200

    Added an animated icon to the GitHub repository

--- src/App.svelte
+++ src/App.svelte
@@ -7,24 +7,35 @@
 	import Certifications from "./components/Certifications.svelte";
 	import Experiences from "./components/Experiences.svelte";
 	import Educations from "./components/Educations.svelte";
+	import GitHubCorner from "./components/GitHubCorner.svelte";
 </script>
 
 <main lang={config.locale}>
+	<!-- Logo GitHub -->
+	<GitHubCorner />
+
+	<!-- En-tête de la page -->
 	<Header />
+
+	<!-- Profil de la personne -->
 	<Profile />
 
+	<!-- Compétences de la personne -->
 	{#if config.skills?.length > 0}
 		<Skills />
 	{/if}
 
+	<!-- Certifications obtenues -->
 	{#if config.certifications?.length > 0}
 		<Certifications />
 	{/if}
 
+	<!-- Expériences professionnelles -->
 	{#if config.experiences?.length > 0}
 		<Experiences />
 	{/if}
 
+	<!-- Formations scolaires -->
 	{#if config.educations?.length > 0}
 		<Educations />
 	{/if}
