e915c44e93c46a93c960fc6e1e95137e2a487aa2
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-05-04 08:49:09 +0200

    Added hobby support through a custom Svelte component

---
+++ src/components/Hobbies.svelte
@@ -0,0 +1,19 @@
+<script>
+	// Importation des dépendances et composants.
+	import config from "../data/default.json";
+</script>
+
+<section>
+	<h2>Loisirs</h2>
+
+	{#each config.hobbies as hobby ( hobby )}
+		<p>{@html hobby}</p>
+	{/each}
+</section>
+
+<style>
+	p {
+		/* Description des loisirs */
+		font-size: 0.875rem;
+	}
+</style>
